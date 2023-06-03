6629e28138d0d5a2df150884fffe858efb967d10
Alexandr <47576452+Shuregg@users.noreply.github.com>
2023-06-03 18:55:12 +0300

    taking the average into a separate function

--- (L1) Statistical processing of a one-dimensional sample/samplingVariances.m
+++ (L1) Statistical processing of a one-dimensional sample/samplingVariances.m
@@ -3,24 +3,15 @@ function [S2_n, S2_0] = samplingVariances(X, P)
     P_size = size(P);
     
     if(X_size(1) == P_size(1) && X_size(2) == P_size(2))
-       
        n = max(X_size);
-
-       x_average = 0;
-       for i = 1:1:n
-           x_average = x_average + X(i);
-       end
-       x_average = x_average / n;
-       
+       x_average = average(X);
        S2_n = 0;
+       
        for i = 1:1:n
           S2_n = S2_n + (X(i) - x_average)^2;
        end
-       
        S2_0 = S2_n / (n-1);
        S2_n = S2_n / n;
-       
-       
     else
         disp('Wrong size of matrix!')
         size(X)
